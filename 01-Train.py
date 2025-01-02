3c1ed8a99012d37ad777d76489f5b90fa054919d
Yusuf <pratamayusuf3@gmail.com>
2025-01-02 16:51:15 +0700

    practice on 2 jan 2025

--- 01-Train.py
+++ 01-Train.py
@@ -10,39 +10,63 @@
 # ReverseLetterOnly - DONE
 # Turing2 - DONE
 
-# AddLinkedList - 
+# AddLinkedList - DONE
+# LetterComboOfPhone -
 
 
-def main():
-    # Input
-    k = [1,2,2,1,5,3,4,3,4]
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def addLinkedList(l1,l2):
+    dummy_head = ListNode(0)
+    tail = dummy_head 
+    carry = 0
     
-    result = 0
-    
-    # Fungsi Hasil
-    result =  solution(k)
-    
-    # Print result
-    print("Hasil : ", result)
-    
-    
-def solution(k):
-    
-    if len(k) < 3 :
-        return -1
-    
-    k.sort()
-    
-    for i in range (0, len(k), 2):
+    while l1 is not None or l2 is not None or carry != 0 :
+        digit1 = l1.val if l1 is not None else 0
+        digit2 = l2.val if l2 is not None else 0
         
-        if i == (len(k) - 1) :
-            return k[i]
+        total = digit1 + digit2 + carry
+        carry = total // 10
+        new_digit = total % 10
         
-        elif k[i] != k[i+1]:
-            return k[i]
+        tail.next = ListNode(new_digit)
+        tail = tail.next
+        
+        l1 = l1.next if l1 is not None else None 
+        l2 = l2.next if l2 is not None else None 
     
-    return -1
+    return dummy_head.next    
+
+        
+def printLinkedList(head):
+    current = head
+    while current is not None:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("null")
 
 
 if __name__ == "__main__":
-    main()
+    print("Program Dimulai")
+    
+    # Membuat 2 linked list
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+    
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
+    
+    # Menghitung hasil penjumlahan dua linked list
+    result = addLinkedList(l1,l2)
+    
+    # Mencetak Hasil
+    print("Hasil :")
+    printLinkedList(result)
+    
+    print("Program Selesai")
