3fa20ce2189e11c9710a3a5d7beb1ce3d002fb60
Yusuf <pratamayusuf3@gmail.com>
2025-02-07 09:44:41 +0700

    practice on 7 feb 2025

--- 01-Train.py
+++ 01-Train.py
@@ -4,8 +4,8 @@
 # ReverseInteger - DONE
 # Turing1 - DONE
 
-# 4Sum - 
-# IntToRoman - DONE
+# 4Sum - DONE
+# IntToRoman - 
 # MedianTwoSortedArrays - DONE
 # ReverseLetterOnly - DONE
 # Turing2 - DONE
@@ -29,24 +29,54 @@
 # ZigZag - DONE
 
 
-
-def solution(k):
-    k.sort()
-    result = 0
+def fourSum(nums, target):
     
-    if len(k) < 2:
-        return -1
+    nums.sort()
+    result = []
     
-    for i in range (0, len(k),2):
-        result += k[i]
+    for i in range (len(nums)-3):
         
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        
+        for j in range (i+1, len(nums) - 2):
+            
+            if j > i+1 and nums[j] == nums[j-1]:
+                continue
+            
+            left = j + 1
+            right = len(nums) - 1
+            
+            while left < right:
+                total = nums[i] + nums[j] + nums[left] + nums[right]
+                
+                if total == target:
+                    result.append([nums[i], nums[j], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    
+                    if left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    if left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                        
+                elif total < target:
+                    left += 1
+                    
+                else:
+                    right -= 1
     return result
+        
 
-# Input
-k = [1,4,3,2]
 
-# Output 
-result = solution(k)
-
-# Print result
-print("Hasil : ",result)
+if __name__ == "__main__":
+    
+    #nums = [2,4,1,3,2,6,5,8,7]
+    #target = 10
+    
+    nums = [1,0,-1,0,-2,2]
+    target = 0
+    
+    result = fourSum(nums, target)
+    
+    print("Hasil : ", result)
