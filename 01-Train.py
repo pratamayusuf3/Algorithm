14d850c542e879f9c97ef114b0a923dc3a2fe89c
Yusuf <pratamayusuf3@gmail.com>
2025-02-09 14:36:58 +0700

    practice on 8 - 9 feb 2025

--- 01-Train.py
+++ 01-Train.py
@@ -5,9 +5,9 @@
 # Turing1 - DONE
 
 # 4Sum - DONE
-# IntToRoman - 
+# IntToRoman - DONE
 # MedianTwoSortedArrays - DONE
-# ReverseLetterOnly - DONE
+# ReverseLetterOnly - 
 # Turing2 - DONE
 
 # AddLinkedList - DONE
@@ -29,54 +29,20 @@
 # ZigZag - DONE
 
 
-def fourSum(nums, target):
+def solution(nums1, nums2):
+    comb_nums = nums1 + nums2
+    comb_nums.sort()
     
-    nums.sort()
-    result = []
-    
-    for i in range (len(nums)-3):
-        
-        if i > 0 and nums[i] == nums[i-1]:
-            continue
-        
-        for j in range (i+1, len(nums) - 2):
-            
-            if j > i+1 and nums[j] == nums[j-1]:
-                continue
-            
-            left = j + 1
-            right = len(nums) - 1
-            
-            while left < right:
-                total = nums[i] + nums[j] + nums[left] + nums[right]
-                
-                if total == target:
-                    result.append([nums[i], nums[j], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    
-                    if left < right and nums[left] == nums[left-1]:
-                        left += 1
-                    if left < right and nums[right] == nums[right-1]:
-                        right -= 1
-                        
-                elif total < target:
-                    left += 1
-                    
-                else:
-                    right -= 1
-    return result
-        
+    if len(comb_nums) % 2 == 1:
+        return comb_nums[len(comb_nums) // 2]
+    else:
+        var1 = comb_nums[len(comb_nums) // 2]
+        var2 = comb_nums[(len(comb_nums) // 2) - 1]
+        return (var1 + var2) / 2.0
 
+# Main Program
+nums1 = [1,3,4]
+nums2 = [2,5]
 
-if __name__ == "__main__":
-    
-    #nums = [2,4,1,3,2,6,5,8,7]
-    #target = 10
-    
-    nums = [1,0,-1,0,-2,2]
-    target = 0
-    
-    result = fourSum(nums, target)
-    
-    print("Hasil : ", result)
+result = solution(nums1,nums2)
+print("Hasil : ",result)
