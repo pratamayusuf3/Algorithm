14fe9477707674bedc3e4414c8deec353113d007
Yusuf <pratamayusuf3@gmail.com>
2025-02-11 08:34:32 +0700

    practice on 11 feb 2025

--- 01-Train.py
+++ 01-Train.py
@@ -8,9 +8,9 @@
 # IntToRoman - DONE
 # MedianTwoSortedArrays - DONE
 # ReverseLetterOnly - DONE
-# Turing2 - 
+# Turing2 - DONE
 
-# AddLinkedList - DONE
+# AddLinkedList - 
 # LetterComboOfPhone - DONE
 # PalindromeNumber - DONE
 # RomanToInt - DONE
@@ -29,30 +29,30 @@
 # ZigZag - DONE
 
 
+def main():
+    # Input
+    k = [1,2,2,1,5,5,4,3,3,4]
+    
+    result = 0
+    
+    # Fungsi Hasil
+    result =  solution(k)
+    
+    # Print result
+    print("Hasil : ", result)
+    
+    
 def solution(k):
-    list_k = list(k)
-    
-    left = 0
-    right = len(list_k) - 1
-    
-    while left < right:
-        if not list_k[left].isalpha():
-            left += 1
-        elif not list_k[right].isalpha():
-            right -= 1
-        else:
-            list_k[left], list_k[right] = list_k[right], list_k[left]
-            left += 1
-            right -= 1
-            
-    return "".join(list_k)
-    
+    k.sort()
+   
+    for i in range (0,len(k),2):
+       if i == len(k)-1:
+           return k[i]
+       elif k[i] != k[i+1]:
+           return k[i]
 
-# Input 
-k = "= Hello % World !?!"
+    return -1
 
-# Output
-result = solution(k)    
 
-# Print result
-print("Hasil : ", result)
+if __name__ == "__main__":
+    main()
